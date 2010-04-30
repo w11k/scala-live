47b279604f3b6d965af22bb13219b55615fab2b4
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-04-30 11:17:50 +0200

    1.5: Made class Person's class parameters firstName and lastName to vals.

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 
-class Person(firstName: String = "", lastName: String = "") {
+class Person(val firstName: String = "", val lastName: String = "") {
   lazy val name = "%s %s".format(firstName, lastName).trim
   override def toString = name
 }
