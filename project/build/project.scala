53bcb04959a52798cc5550c44b0bef40f377b3e6
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-03-24 09:34:34 +0100

    Upgraded to specs 1.6.7.1.

--- project/build/project.scala
+++ project/build/project.scala
@@ -7,6 +7,6 @@ class ScalaLiveProject(info: ProjectInfo) extends DefaultProject(info) {
   // Dependencies (provided)
 
   // Dependencies (test)
-  lazy val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources
+  lazy val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.1" % "test" withSources
   lazy val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
 }
